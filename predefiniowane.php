fd776ed3785a22651898730a7041602e8c84de96
ozgaB <sqidix@gmail.com>
2021-02-08 17:19:14 +0100

    zmiany2

--- predefiniowane.php
+++ predefiniowane.php
@@ -1,5 +1,26 @@
 <html>
-    <head></head>
+    <head>
+        <?php
+            if (isset($_GET['page'])){
+        $allowed_pages = array("java", "php", "mysql");
+        
+        $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);
+        if (in_array($page, $allowed_pages))
+        
+        if (!empty($page)){
+            if (is_file($page."title.php"))
+            {
+                    include($page."title.php");
+            }
+       
+        }
+
+    }else{
+    include("start.php");
+    }
+        ?>
+        
+    </head>
     <body>
         <div id="menu">
             <a href="?page=java">Glowna<a/>
