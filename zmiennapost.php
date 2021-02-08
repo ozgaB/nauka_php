ee4d76d66a88523b941ad6e2d7e3837184281c17
ozgaB <sqidix@gmail.com>
2021-02-08 20:39:51 +0100

    porzadek

---
+++ zmiennapost.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        
+        
+        
+    </head>
+    <body>
+        <form action="odbierz.php" method="post" enctype="multipart/formdata">
+            <div>
+                <div>
+                    Login: <input type="text" name="login" maxlength="8" size="5">
+                </div>
+                <div>
+                    Password: <input type="text" name="password" maxlength="8" size="5">
+                </div>
+                <div>
+                    <input type="submit" value="zaloguj sie"/>
+                </div>
+                
+                
+                
+                
+                
+                
+                
+            </div>
+            
+            
+            
+            
+            
+        </form>
+        
+        
+        
+    </body>
+    
+    
+    
+</html>
+
+
+<?php
+?>
+
