cea61cfcc237a394bd51b8d3d85d18cbf5c90a02
ozgaB <sqidix@gmail.com>
2021-02-09 14:42:21 +0100

    sesje

---
+++ system_sesji/komunikatlogowania.php
@@ -0,0 +1,5 @@
+<?php
+             echo "logowanie powiodlo sie na konto ".$_SESSION['login'];
+         echo "<a href='sesja.php'>odswiez</a>";
+         echo "<a href='sesja.php?akcja=wyloguj'>wyloguj</a>";
+?>
---
+++ system_sesji/sesja.php
@@ -0,0 +1,115 @@
+<?php
+ session_start();
+ 
+ if(!isset($_SESSION['initiate']))
+ {
+     session_regenerate_id();
+     $new_session_id = session_id();
+     session_write_close();
+     session_id($new_session_id);
+     session_start();
+     $_SESSION['initiate'] = 1;
+ }
+ 
+?>
+
+<html>
+    <head>
+        
+        
+        
+    </head>
+    <body>
+        <?php
+        if(isset($_GET['akcja']) && $_GET['akcja'] == "wyloguj")
+{
+             $_SESSION['zalogowany'] = 0;
+             session_destroy();
+echo "wylogowano";
+} 
+
+
+
+if ($_SESSION['zalogowany'] == 1 && time() - $_SESSION['time'] > 5*60)
+{
+    echo "zostajesz wylogowany";
+    $_SESSION['zalogowany'] == 0;
+    session_destroy();
+}
+/*if ($_SESSION['zalogowany'] == 1 && $_SESSION['komp_info'] != $_SERVER['HTTP_USER_AGENT']) // Zmiana przeglądarki zabezpieczenie
+{
+    echo "zostajesz wylogowany z powodu zmiany";
+    $_SESSION['zalogowany'] == 0;
+    session_destroy();
+} */
+if((isset($_POST['login']) && $_POST['password']) or $_SESSION['zalogowany']== 1)
+{
+    if((!empty($_POST['login']) && !empty($_POST['password'])) or $_SESSION['zalogowany']== 1)
+    {
+        if($_SESSION['logowanie'] == 0){
+     $login = filter_var($_POST['login'], FILTER_SANITIZE_STRING ) ;
+     $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+        }
+     if($login == "Bartek" && $password == "haslo" || $_SESSION['zalogowany'] == 1)
+     {
+         if($_SESSION['zalogowany'] == 0)
+             $_SESSION['login'] = $login;
+             
+         //  include tutaj może być czy coś
+            include('komunikatlogowania.php');
+         $_SESSION['zalogowany'] = 1;
+         $_SESSION['time'] = time();
+         $_SESSION['comp_info'] = $_SERVER['HTTP_USER_AGENT'];
+     }
+     else
+         bladlogowania();
+    }
+    else
+        bladlogowania();
+}
+
+function bladlogowania()
+{
+    echo  "logowanie nie powiodlo sie <a href='sesja.php'>Powrót</a>";
+}
+if ($_SESSION['zalogowany'] == 0){
+    ?>
+
+        <form action="sesja.php" method="post" enctype="multipart/formdata">
+            <div>
+                <div>
+                    Login: <input type="text" name="login" maxlength="8" size="5">
+                </div>
+                <div>
+                    Password: <input type="password" name="password" maxlength="8" size="5">
+                </div>
+                <div>
+                    <input type="submit" value="zaloguj sie"/>
+                </div>
+                
+                
+                
+                
+                
+                
+                
+            </div>
+            
+            
+            
+            
+            
+        </form>
+  <?php
+}
+?>
+        
+        
+    </body>
+    
+    
+    
+</html>
+
+
+
